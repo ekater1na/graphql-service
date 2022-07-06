c2dcc14211b961da5c31fcb5098b4a502f5351e2
ekater1na <ekaterina_l@tut.by>
2022-07-06 13:34:39 +0300

    build: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistsModule } from './artists/artists.module';
+import { GenresModule } from './genres/genres.module';
+import { BandsModule } from './bands/bands.module';
+import { TracksModule } from './tracks/tracks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ArtistsModule,
+    GenresModule,
+    BandsModule,
+    TracksModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
