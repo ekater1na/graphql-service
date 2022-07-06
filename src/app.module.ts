28cf935af45e5ec2c064fbe843f3d32f7217c357
ekater1na <ekaterina_l@tut.by>
2022-07-06 13:47:59 +0300

    feat: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ArtistsModule } from './artists/artists.module';
-import { GenresModule } from './genres/genres.module';
-import { BandsModule } from './bands/bands.module';
-import { TracksModule } from './tracks/tracks.module';
-import { UsersModule } from './users/users.module';
+
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ArtistsModule } from './modules/artists/artists.module';
+import { BandsModule } from './modules/bands/bands.module';
+import { GenresModule } from './modules/genres/genres.module';
+import { TracksModule } from './modules/tracks/tracks.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ArtistsModule,
     GenresModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+      playground: true,
+    }),
     BandsModule,
     TracksModule,
     UsersModule,
